c36e50494136057883874e4f15b1a08941084fa2
lfenix_rust <lfenix_rust@lfenix-youtubes-Mac-mini.local>
2023-12-31 12:12:49 -0600

    added (fixed) 6.2, updated sharding database code

--- examples/6_2_sharding_database_to_achieve_shared_state.rs
+++ examples/6_2_sharding_database_to_achieve_shared_state.rs
@@ -1,19 +1,45 @@
+/*
+Another possible strategy that can be used to share the state of a database is "Sharding"
+This involves breaking the database up into Shards, or individual pieces, and using hashing to determine which shard should contain which key
+Explained below
+*/
+
 use bytes::Bytes;
+
 use mini_redis::{Connection, Frame};
+use mini_redis::Command::{self, Get, Set};
+
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+
 use tokio::net::{TcpListener, TcpStream};
 
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+type ShardedDb = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
 /*
-Here we are specifying the object that will be shared among multiple threads
-(The database)
-    By using "type", we avoid having to write the whole thing out every time and can just specify 'Db' whenever we need the whole type
-It can be passed and owned by multiple threads without going out of scope, because of Arc
-It can be modified from multiple threads, because of Mutex
-Within the hashmap, we are using Strings as keys, and Bytes (mentioned in detail in the cargo.toml) as values
-    In summary, Bytes are like vector byte arrays (Vec<u8>) but better :D
+Here, we create the type of the Sharded database
+The Arc is so this database may be owned by multiple threads/tasks
+The outer Vec holds all of the individual shards that comprise the database
+Each individual shard is held in a Mutex, so that it may be modified from multiple threads
+    This is of course unlikely since there are so many shards and few connections, but for scaling, as connections increase, we can increase the number of shards
+    The Mutex is there to handle this situation
+The HashMap IS the individual shard, within which keys are of type "String" and values are of type Bytes
+    Bytes work similarly to Vec<u8> but they can be passed around easily between threads (and owned by multiple threads)
+    Bytes type is roughly an Arc<Vec<u8>> but with some added capabilities
 */
+
+fn new_sharded_db(num_shards: usize) -> ShardedDb {
+    let mut db = Vec::with_capacity(num_shards);
+    //A sharded database must have a specified number of shards on creation. This is specified in the function parameter above
+    for _ in 0..num_shards {
+        db.push(Mutex::new(HashMap::new()));
+        //this loop goes about creating the individual shards, the number of which is specified in the function parameter
+    }
+    Arc::new(db)
+    //the newly created databse is returned in an Arc so it can be shared across multiple threads/tasks
+}
 
 #[tokio::main]
 async fn main() {
@@ -21,56 +47,67 @@ async fn main() {
 
     println!("Listening");
 
-    let db = Arc::new(Mutex::new(HashMap::new()));
-    //creates a Hashmap instance (our database, or DB, remember) that can be passed around to various threads (Arc) and modified in various threads (Mutex)
-    //The instance starts of empty
-    //remember also that the Mutex being used is from std, not tokio
-    //this causes whatever thread holds the lock of the Mutex to pause execution until the lock is released
-    //from documentation - However, switching to tokio::sync::Mutex usually does not help as the asynchronous mutex uses a synchronous mutex internally.
+    let db = new_sharded_db(1000);
+    //here we initialize the sharded database, with 1000 shards
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         // Clone the handle to the hash map.
         let db = db.clone();
-        // we create a copy to be given to the spawned task that will perform the database query (read or update)
+        // "Clone" or shallow copies the database to pass onto a newly spawned task for every connection (possible due to Arc)
 
         println!("Accepted");
         tokio::spawn(async move {
             process(socket, db).await;
-            // the db instance is given over to the spawned task, which then uses it to perform DB operation
         });
     }
-
-    //Of course, this strategy has a disadvantage: if one thread holds the lock, all other threads are blocked until the one thread releases the lock
-    //This is called Contention, and it is a problem, especially with scaling applications
 }
 
-async fn process(socket: TcpStream, db: Db) {
-    use mini_redis::Command::{self, Get, Set};
-
-    // Connection, provided by `mini-redis`, handles parsing frames from
-    // the socket
+async fn process(socket: TcpStream, db: ShardedDb) {
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                let mut db = db.lock().unwrap();
-                //this line puts a lock on the passed database instance from main so that we can modify it here
-                db.insert(cmd.key().to_string(), cmd.value().clone());
-                //this line makes the modification (inserts a value) into the database
-                //note that it clones the value inside the cmd using the bytes library
-                //this clone does not make a full copy of what is inside cmd.value(), rather, it allows for both the database and the cmd to own it concurrently due to the nature of Bytes
-                //this is more efficient than making a copy
+                let key = cmd.key().to_string();
+                let value = cmd.value().clone();
+
+                let mut hasher = DefaultHasher::new();
+                key.hash(&mut hasher);
+                let shard = (hasher.finish() as usize) % db.len();
+                //124884239582 % 1000 = 582
+                //9 % 1000 = 9
+                //124903 % 1000 = 903
+
+                /*
+                Explanation of a hasher
+                The reason for a hasher is to turn the key into a u64 (We take the key that is received from the connection, hasher takes this key (key.hash and then hasher.finish) and turns it into a unique u64 value
+                We then divide this value by the length of db and take the remainder (modulus)
+                    Recall that this is the number of created shards
+                The result is a value between 0 and the number of shards (1000 in this case, so 0 - 999)
+                This value is used to assign which shard this key will be assigned to
+                This saves time/computational power because every key will always give the same u64 when hashed
+                This value can then be used to find out EXACTLY which shard this key is being stored in
+                 */
+
+                let mut db_shard = db[shard].lock().unwrap();
+                db_shard.insert(key, value);
+                //after the above, we then unlock the shard, input the key value pair into that shard and then deallocate the lock
+
                 Frame::Simple("OK".to_string())
-                //Lock is released, allowing other tasks to read/modify
             }
             Get(cmd) => {
-                let db = db.lock().unwrap();
-                //db is locked so that the database can be read without it also concurrently being changed elsewhere
-                if let Some(value) = db.get(cmd.key()) {
+                let key = cmd.key().to_string();
+
+                let mut hasher = DefaultHasher::new();
+                key.hash(&mut hasher);
+                let shard = (hasher.finish() as usize) % db.len();
+
+                //Similar to what happened above, we use the unique u64 value of the hashed key to identify exactly which shard to look in to retrieve the value, rather than having to loop through all of them
+
+                let db_shard = db[shard].lock().unwrap();
+                if let Some(value) = db_shard.get(&key) {
                     Frame::Bulk(value.clone())
-                    //cloned via Bytes crate again, which allows for multiple ownership and avoids making multiple copies
                 } else {
                     Frame::Null
                 }
